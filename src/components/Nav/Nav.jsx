3ad4cea365d43727922b9b7ffcf36c6782ed260e
christanfox <bayshorestinks@aol.com>
2022-06-19 16:34:37 -0400

    commented out services component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import './nav.css'
 import {RiHome5Line} from 'react-icons/ri'
 import {RiUser3Line} from 'react-icons/ri'
 import {RiBook2Line} from 'react-icons/ri'
-import {RiServiceLine} from 'react-icons/ri'
+// import {RiServiceLine} from 'react-icons/ri'
 import {RiBriefcaseLine} from 'react-icons/ri'
 import {RiMessage3Line} from 'react-icons/ri'
 import { useState } from 'react'
@@ -15,7 +15,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><RiHome5Line/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUser3Line/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><RiBook2Line/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      {/* <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a> */}
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiBriefcaseLine/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage3Line/></a>
     </nav>
