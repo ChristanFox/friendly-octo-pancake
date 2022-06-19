db1e83ab388b80826d943e5de6b8271f03a56b24
christanfox <bayshorestinks@aol.com>
2022-06-19 15:13:03 -0400

    completed the contact-me component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {RiWhatsappLine} from 'react-icons/ri'
 import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>christanfox@gmail.com</h5>
+            <a href="mailto:testing@test.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>testingskils</h5>
+            <a href="https://m.me/ChristanFox" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiWhatsappLine className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>123456789</h5>
+            <a href="https://api.whatsapp.com/send?phone=+3472365129" target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
