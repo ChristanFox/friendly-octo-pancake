f1bd533086cf6a768007354f897b5da26fd76601
christanfox <bayshorestinks@aol.com>
2022-06-19 16:10:54 -0400

    completed the footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import {AiOutlineFacebook} from 'react-icons/ai'
+import {GrInstagram} from 'react-icons/gr'
+import {RiTwitterLine} from 'react-icons/ri'
 import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="" className='footer__logo'>Christan Fox</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Expeirience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com"><AiOutlineFacebook/></a>
+        <a href="https://instagram.com"><GrInstagram/></a>
+        <a href="https://twitter.com"><RiTwitterLine/></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Christan Fox. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
