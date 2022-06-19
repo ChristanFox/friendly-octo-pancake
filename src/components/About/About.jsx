cc79ae8ea54add03e3c0ac4b1172e6d0ee9694a1
christanfox <bayshorestinks@aol.com>
2022-06-19 11:12:04 -0400

    completed the experiences component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,9 +3,11 @@ import ME from '../../assets/me-about.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
+import { useState } from 'react'
 import './about.css';
 
 const About = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <section id='about'>
       <h5>Get To Know</h5>
@@ -39,7 +41,7 @@ const About = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque eos voluptatem eius dolorem maiores nihil ducimus at rem ullam reprehenderit quidem quia deserunt, molestiae, eligendi amet repellat molestias quos totam.
           </p>
 
-          <a href="#contact" className='btn'>Let's Talk</a>
+          <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Let's Talk</a>
         </div>
       </div>
     </section>
