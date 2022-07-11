9aba556257207a1b4004bb91bff2f52f2eba2f6b
christanfox <bayshorestinks@aol.com>
2022-07-10 20:08:53 -0400

    reinstalled node modules

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/cv-ChristanFox.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn'>Reach Me</a>
     </div>
   )
